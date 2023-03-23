f8ef822c0da99c87a99c95033eac05aa72f04932
Joseph Earl <sakugastreet@gmail.com>
2023-03-22 19:20:54 -0600

    finished projects

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Gay Bowser!");
+
+        string first_name;
+        string last_name;
+
+        Console.WriteLine("what is your name?");
+        first_name = Console.ReadLine();
+        Console.WriteLine("what is your last name?");
+        last_name = Console.ReadLine();
+        Console.WriteLine($"It is nice meet you {last_name}, {first_name} {last_name}.");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your Grade?");
+        string user_input = Console.ReadLine();
+        int value = int.Parse(user_input);
+
+        
+        string grade_letter;
+        bool passed = true;
+
+        if (value > 90)
+        {
+            grade_letter = "A";
+        }
+        else if (value > 80)
+        {
+            grade_letter = "B";
+        }
+        else if (value > 70)
+        {
+            grade_letter = "C";
+        }
+        else if (value > 60)
+        {
+            grade_letter = "D";
+            passed = false;
+        }
+        else
+        {
+            grade_letter = "F";
+            passed = false;
+        }
+
+        
+        if (passed)
+        {
+            Console.WriteLine($" with a grade of {value}, you passed with a grade {grade_letter}.");
+        }
+        else
+        {
+            Console.WriteLine($" with a grade of {value}, you failed with a grade {grade_letter}.");
+        }
     }
 }
